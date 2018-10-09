3968c9b1f58c1db4f2c6d8c9449a356085453e0b
weiqing-nic <yoweiqing@gmail.com>
2018-10-09 22:10:05 +0800

    Increase the length of uniqueId

--- src/main/java/seedu/address/model/person/UniqueId.java
+++ src/main/java/seedu/address/model/person/UniqueId.java
@@ -34,7 +34,15 @@ public class UniqueId {
         Random random = new Random();
         StringBuilder buffer = new StringBuilder(targetStringLength);
         for (int i = 0; i < targetStringLength; i++) {
-            char randomLimitedInt = (char) ('A' + (random.nextInt(20)));
+
+            char randomLimitedInt;
+
+            if (i % 2 == 0 ) {
+                randomLimitedInt = (char) ('A' + (random.nextInt(26)));
+            } else {
+                randomLimitedInt = (char) ('a' + (random.nextInt(26)));
+            }
+
             buffer.append( randomLimitedInt);
         }
         this.value = buffer.toString();
